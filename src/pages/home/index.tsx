a22400ae4ba5e077af3bc8bd1ac5bb9f9538de97
Rharhuky <rharhuky@gmail.com>
2025-04-05 15:46:59 -0300

    feat: layout home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
-    <div className="w-full h-screen bg-amber-300">
-      <h1 className="text-black text-3xl">Home</h1>
+    <div className="flex flex-col w-full py-4 items-center justify-center">
+      <h1 className="md:text-4xl  text-3xl font-bold text-white mt-20">
+        Rharhuandrew
+      </h1>
+      <span className="text-gray-400 mb-5 mt-3">See my links!</span>
+      <main className="flex flex-col w-11/12 max-w-xl text-center">
+        <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
+          <a>
+            <p className="text-base md:text-lg">Instagram</p>
+          </a>
+        </section>
+
+        <footer className="flex justify-center gap-3 my-4"></footer>
+      </main>
     </div>
   );
 }
