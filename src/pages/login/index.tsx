d77fb3f95d50a910e5925ea9765c81f2eb543954
Rharhuky <rharhuky@gmail.com>
2025-04-05 21:38:51 -0300

    feat: Login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router";
+
 export default function Login() {
   return (
-    <div>
-      <h1>Página de Login</h1>
+    <div className="flex flex-col justify-center items-center h-screen w-full">
+      <Link to={"/"}>
+        <h1 className="text-white mt-11 mb-7 font-bold text-5xl">
+          Rharh
+          <span className="bg-gradient-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent">
+            uky
+          </span>
+        </h1>
+      </Link>
     </div>
   );
 }
